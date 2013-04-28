5740dfc2540ca44e39d77fcae74df85c4e2b67f2
wjj474957860 <474957860@qq.com>
2013-04-28 16:44:17 +0800

    modified showPic.js == added "comment"

--- js/showPic.js
+++ js/showPic.js
@@ -2,6 +2,10 @@
 function showPic(whichpic){
 	//首先获得该指定图片的href的字符串型字符串
 	var source = whichpic.getAttribute("href");
+	//获得指定图片的title属性的字符串
+	var title = whichpic.getAttribute("title");
 	var placeholder = document.getElementById("placeholder");
+	//以下是追加相对应的属性值
 	placeholder.setAttribute("src",source);
+	placeholder.setAttribute("title",title);
 }
